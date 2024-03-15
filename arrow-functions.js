cd4f6e99442990d2f5c5bcc45432df0f43c2ddae
peter rimaszecsi <hull@localappliancerentals.co.uk>
2024-03-15 09:06:51 +0000

    Add more for spread operator

--- arrow-functions.js
+++ arrow-functions.js
@@ -50,12 +50,24 @@ let arr1 = [1,2,3];
 let arr2 = arr1;
 arr2.push(4);
 console.log('2nd array:', arr2);
-console.log('1st array:' arr1);
+console.log('1st array:', arr1);
 
 // Copying an array
 let arr3 = [5,6,7];
 let arr4 = [...arr3];
 
 arr4.push(7);
-console.log('3rd array:' arr3);
-console.log('4th array:' arr4);
+console.log('3rd array:', arr3);
+console.log('4th array:', arr4);
+
+// Copiyng an object
+let obj1 = {a:1, b:2, c:3};
+let obj2 = {...obj1, d:4};
+let obj3 = {...obj2, b:5};
+console.log('1st object:', obj1);
+console.log('2nd object:', obj2);
+console.log('3rd object:', obj3);
+
+// Copiyng only part of the array
+let arr5 = [...arr1, {...obj1}, ...arr3, 'x', 'y', 'z'];
+console.log(arr5);
