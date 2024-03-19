eac6ce1952a0baca7b9c939479c8d7f2185a5c14
peter rimaszecsi <hull@localappliancerentals.co.uk>
2024-03-19 13:01:16 +0000

    finish tasks

--- arrow-functions.js
+++ arrow-functions.js
@@ -1,5 +1,5 @@
 // /**
-//  * To run this file in Gitpod, use the 
+//  * To run this file in Gitpod, use the
 //  * command node arrow-functions.js in the terminal
 //  */
 
@@ -11,7 +11,6 @@
 // }
 // let sum = addTwoNumbers(3, 5)
 // console.log(sum);
-
 
 // // Arrow Function With Parameters
 
@@ -104,7 +103,6 @@
 // let sum14 = sumRest2(1,2,3,4,5,6);
 // console.log("Sum14: ", sum14);
 
-
 //Destructuring Arrays
 
 let ages = [30, 26, 27];
@@ -192,7 +190,6 @@ let ages = [30, 26, 27];
 //     }
 // ];
 
-
 // const makeList = (arr, student) => {
 //     for (let i of arr) {
 //         if (i.name === student) {
@@ -207,8 +204,8 @@ let ages = [30, 26, 27];
 
 // console.log(first, second, rest);
 
-                      //map() method:
-                    //Using loop
+//map() method:
+//Using loop
 // let num = [1,2,3,4,5,6,7,8,9,10];
 // let results = [];
 // for (let i of num) {
@@ -265,7 +262,6 @@ let ages = [30, 26, 27];
 // const studentsWithId = students.map(i => [i.id, i.name]);
 // console.log(studentsWithId);
 
-
 // let students = [
 //   {
 //     name: "John",
@@ -297,7 +293,6 @@ let ages = [30, 26, 27];
 
 // console.log(john, rest);
 
-
 // //filter() method:
 
 //simple filter:
@@ -310,17 +305,17 @@ let ages = [30, 26, 27];
 //     {
 //         name: 'Anna',
 //         age: 25,
-        
+
 //     },
 //     {
 //         name: 'Fran',
 //         age: 35,
-       
+
 //     },
 //     {
 //         name: 'Tom',
 //         age: 22,
-      
+
 //     }
 // ];
 
@@ -334,48 +329,224 @@ let ages = [30, 26, 27];
 
 //complex filter:
 
-const students = [
-    {
-        id: 1,
-        name: 'Mark',
-        profession: 'teacher', 
-        skill: [
-            {subject: 'maths', score: 90},
-            {subject: 'english', score: 80},
-            {subject: 'cad', score: 87},
-        ]
-    },
-    {
-        id: 2,
-        name: 'Elle',
-        profession: 'engineer', 
-        skill: [
-            {subject: 'science', score: 90},
-            {subject: 'english', score: 80},
-            {subject: 'art', score: 87},
-        ]
-    },
-    {
-        id: 3,
-        name: 'John',
-        profession: 'designer', 
-        skill: [
-            {subject: 'art', score: 90},
-            {subject: 'english', score: 80},
-            {subject: 'cad', score: 87},
-        ]
-    },
-    {
-        id: 4,
-        name: 'Anna',
-        profession: 'doctor', 
-        skill: [
-            {subject: 'medicine', score: 90},
-            {subject: 'english', score: 80},
-            {subject: 'cad', score: 87},
-        ]
-    },
+// const students = [
+//     {
+//         id: 1,
+//         name: 'Mark',
+//         profession: 'teacher',
+//         skill: [
+//             {subject: 'maths', score: 90},
+//             {subject: 'english', score: 80},
+//             {subject: 'cad', score: 87},
+//         ]
+//     },
+//     {
+//         id: 2,
+//         name: 'Elle',
+//         profession: 'engineer',
+//         skill: [
+//             {subject: 'science', score: 90},
+//             {subject: 'english', score: 80},
+//             {subject: 'art', score: 87},
+//         ]
+//     },
+//     {
+//         id: 3,
+//         name: 'John',
+//         profession: 'designer',
+//         skill: [
+//             {subject: 'art', score: 90},
+//             {subject: 'english', score: 80},
+//             {subject: 'cad', score: 87},
+//         ]
+//     },
+//     {
+//         id: 4,
+//         name: 'Anna',
+//         profession: 'doctor',
+//         skill: [
+//             {subject: 'medicine', score: 90},
+//             {subject: 'english', score: 80},
+//             {subject: 'cad', score: 87},
+//         ]
+//     },
+// ];
+// // filter skills with score > 85
+// const filteredSkills = students.map(i => i.skill.filter(j => j.score > 85));
+// console.log(filteredSkills);
+
+// let students = [
+//   {
+//     name: "John",
+//     subjects: ["maths", "english", "cad"],
+//     teacher: { maths: "Harry", english: "Joan", cad: "Paul" },
+//     results: { maths: 90, english: 75, cad: 87 },
+//   },
+//   {
+//     name: "Emily",
+//     subjects: ["science", "english", "art"],
+//     teacher: { science: "Iris", english: "Joan", art: "Simon" },
+//     results: { science: 93, english: 80, art: 95 },
+//   },
+//   {
+//     name: "Adam",
+//     subjects: ["science", "maths", "art"],
+//     teacher: { science: "Iris", maths: "Harry", art: "Simon" },
+//     results: { science: 84, maths: 97, art: 95 },
+//   },
+//   {
+//     name: "Fran",
+//     subjects: ["science", "english", "art"],
+//     teacher: { science: "Iris", english: "Joan", art: "Simon" },
+//     results: { science: 67, english: 87, art: 95 },
+//   },
+// ];
+
+// //You will create a single line of code that should filter the students array and return only the items whos maths scores are greater than or equal to  90.  As this needs to be done in a single line of code, you will also be required to use an arrow function that does not have a return statement.
+
+// const topMaths = students.filter(student => student.results.maths >= 90);
+// console.log(topMaths);
+
+// //reduce() method:
+//summing numbers:
+// const num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// let sum = num.reduce((acc, cur) => {
+//   console.log("accumulator:", acc, "current:", cur, "total:", acc + cur);
+//   return acc + cur;
+// }, 0);
+
+// console.log(sum);
+
+// const teamMembers = [
+//   {
+//     name: "John",
+//     profession: "designer",
+//     skill: "art",
+//     yrsExperience: 5,
+//   },
+//   {
+//     name: "Anna",
+//     profession: "doctor",
+//     skill: "medicine",
+//     yrsExperience: 10,
+//   },
+//   {
+//     name: "Tom",
+//     profession: "designer",
+//     skill: "maths",
+//     yrsExperience: 7,
+//   },
+//   {
+//     name: "Elle",
+//     profession: "doctor",
+//     skill: "science",
+//     yrsExperience: 8,
+//   },
+// ];
+// // names of all team members by proffession:
+
+// let teamMembersByProfession = teamMembers.reduce((acc, cur) => {
+//   let key = cur.profession;
+//   if (!acc[key]) {
+//     acc[key] = [];
+//   }
+//   acc[key].push(cur.name);
+//   return acc;
+// }, {});
+
+// console.log(teamMembersByProfession);
+
+// let designers = teamMembersByProfession.designer;
+// console.log(designers);
+
+// // totaling a specific object property:
+// let totalExperience = teamMembers.reduce((acc, cur) => acc + cur.yrsExperience, 0);
+// console.log(totalExperience);
+
+// // Grouping by a property, and totaling the grouped values:
+
+// let experienceByProfession = teamMembers.reduce((acc, cur) => {
+//   let key = cur.profession;
+//   if (!acc[key]) {
+//     acc[key] = 0;
+//   }
+//   acc[key] += cur.yrsExperience;
+//   return acc;
+// }, {});
+
+// console.log(experienceByProfession);
+
+
+let students = [
+  {
+    name: "John",
+    subjects: ["maths", "english", "cad"],
+    teacher: { maths: "Harry", english: "Joan", cad: "Paul" },
+    results: { maths: 90, english: 75, cad: 87 },
+  },
+  {
+    name: "Emily",
+    subjects: ["science", "english", "art"],
+    teacher: { science: "Iris", english: "Joan", art: "Simon" },
+    results: { science: 93, english: 73, art: 95 },
+  },
+  {
+    name: "Adam",
+    subjects: ["science", "maths", "art"],
+    teacher: { science: "Iris", maths: "Harry", art: "Simon" },
+    results: { science: 93, english: 88, maths: 97, art: 95 },
+  },
+  {
+    name: "Fran",
+    subjects: ["science", "english", "art"],
+    teacher: { science: "Iris", english: "Joan", art: "Simon" },
+    results: { science: 93, english: 87, art: 95 },
+  },
 ];
-// filter skills with score > 85
-const filteredSkills = students.map(i => i.skill.filter(j => j.score > 85));
-console.log(filteredSkills);
+
+//return the value of students.name and the value of students.results.english whose students.results.english is the highest
+
+const highestEnglishScore = (acc, current) => {
+  const {name: name, results: { english: english },} = current;
+  if (acc === null) {
+    return { name: name, max: english };
+  }
+  if (english > acc.max) {
+    return { name: name, max: english };
+  }
+  return acc;
+};
+const biggest = students.reduce(highestEnglishScore, {name: null, max: null});
+console.log(biggest);
+
+
+
+// const highestEnglishScore = (acc, current) => {
+//   const {
+//     name: currentName,
+//     results: { english: currentEnglish },
+//   } = current;
+//   // Check if acc is null (initial value) or if the current English score is greater than the acc's score
+//   if (acc === null || currentEnglish > acc.score) {
+//     return { name: currentName, score: currentEnglish };
+//   }
+//   return acc;
+// };
+
+// // Use reduce method on students array with highestEnglishScore function, initializing with null
+// const biggest = students.reduce(highestEnglishScore, null);
+
+// console.log(biggest);
+
+
+
+
+// let biggest = students.reduce((acc, cur) => {
+//   if (acc.results.english > cur.results.english) {
+//     return acc;
+//   } else {
+//     return cur;
+//   }
+// }, students[0]);
+// console.log(biggest.name, biggest.results.english);
+
